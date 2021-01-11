79c8ebee8867d73b0a1491fe018a6be0f5f30601
Teja Indupalli <indupalliteja79@gmail.com>
2021-01-11 15:22:39 -0600

    first commit

---
+++ Cdemo.java
@@ -0,0 +1,19 @@
+class Cdemo{
+	
+	public static void main (String x[])
+	{
+		for(int i=1;i<6;i++)
+		{
+		  System.out.println("hi");
+		  System.out.println("hello");
+	       System.out.println("i");	
+		}
+		
+		
+		
+		
+	}
+	
+	
+
+	}
